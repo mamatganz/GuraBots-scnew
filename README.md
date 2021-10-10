bae094cd9236feaaba990ea4f77daa8af1f23b0f
mamatganz <mantanputusbro@gmail.com>
2021-10-10 23:54:16 +0700

    Gurawa

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bot-Skylark
+Skylark bot
+subcribe hazimkn
---
+++ message/help.js
@@ -0,0 +1,473 @@
+exports.gameMenu = (prefix) => {
+    return `
+┏━━━━『 𝐆𝐀𝐌𝐄 𝐌𝐄𝐍𝐔  』━━━━━
+┃ 
+┃       『 𝐎𝐍𝐋𝐘 𝐅𝐎𝐑 𝐆𝐑𝐎𝐔𝐏 』
+┃
+┣➤ #gelud @tag
+┃gelud User
+┃➥ Penggunaan : *#gelud 
+┃@tag user*
+┃
+┣➤ #tictactoe @tag
+┃Game tictactoe
+┃➥ Penggunaan :  *#tictactoe 
+┃@tag*
+┃
+┣━━━━━━━━━━━━━━━━━━━━━━
+┣➤ #slot
+┃Permainan User
+┃➥ Penggunaan : Slot 
+┃
+┣➤ #siapaaku
+┃Permainan User
+┃➥ Penggunaan : #siapaaku
+┃
+┣➤ #asahotak
+┃Permainan User
+┃➥ Penggunaan : #asahotak
+┃
+┣➤ #tebaklirik
+┃Permainan User
+┃➥ Penggunaan : #tebaklirik
+┃
+┣➤ #tebaklagu
+┃Permainan User
+┃➥ Penggunaan : #tebaklagu
+┃
+┣➤ #tebakkata
+┃Permainan User
+┃➥ Penggunaan : #tebakkata
+┃
+┣➤ #susunkata
+┃Permainan User
+┃➥ Penggunaan : #susunkata
+┃
+┣➤ #caklontong
+┃Permainan User
+┃➥ Penggunaan : #caklontong
+┃
+┣➤ #tebakjenaka
+┃Permainan User
+┃➥ Penggunaan : #tebakjenaka
+┃
+┣➤ #tebaktebakan
+┃Permainan User
+┃➥ Penggunaan : #tebaktebakan
+┃
+┣➤ #tebakgambar
+┃Permainan User
+┃➥ Penggunaan : #tebakgambar
+┃
+┣➤ #suit *batu/kertas/gunting
+┃Permainan User
+┃➥ Penggunaan :*#suit 
+┃ [batu/kertas/gunting]
+┃
+┗━━━━『 𝐆𝐔𝐑𝐀 𝐁𝐎𝐓  』━━━━━
+
+                                               ©𝐂𝐫𝐞𝐚𝐭𝐨𝐫 𝐊𝐞𝐧𝐳𝐚𝐆𝐚𝐧𝐳𝐳
+`
+}
+exports.funMenu = (prefix) => {
+    return `
+┏━━━━『 *𝐅𝐔𝐍 𝐌𝐄𝐍𝐔*  』━━━━━
+┃
+┃    『 _*𝐎𝐍𝐋𝐘 𝐅𝐎𝐑 𝐆𝐑𝐎𝐔𝐏*_ 』
+┃
+┣➤ *#beban*
+┃➥ *Penggunaan* : *#beban*
+┃
+┣➤ *#babi*
+┃➥ *Penggunaan* : *#babi*
+┃
+┃
+┣━━━━━━━━━━━━━━━━━━━━━━━
+┣➤ *#mining*
+┃➥ *Penggunaan* : *#mining*
+┃
+┣➤ *#togel*
+┃➥ *Penggunaan* : *#togel*
+┃
+┣➤ *#cekwatak*
+┃➥ *Penggunaan* : *#cekwatak*
+┃
+┣➤ *#cekmati*
+┃➥ *Penggunaan* : *#cekmati*
+┃
+┣➤ *#wangy*
+┃➥ *Penggunaan* : *#wangy _*nama*_
+┃
+┣➤ *#citacita*
+┃➥ *Penggunaan* : *#citacita*
+┃
+┣➤ *#toxic*
+┃➥ *Penggunaan* : *#toxic*
+┃
+┣➤ *#truth*
+┃➥ *Penggunaan* : *#truth*
+┃
+┣➤ *#dare*
+┃➥ *Penggunaan* : *#dare*
+┃
+┣➤ *#apakah*
+┃➥ *Penggunaan* : *#apakah*
+┃
+┣➤ *#bisakah*
+┃➥ *Penggunaan* : *#bisakah*
+┃
+┣➤ *#kapankah*
+┃➥ *Penggunaan* : *#kapankah*
+┃
+┣➤ *#rate*
+┃➥ *Penggunaan* : *#rate*
+┃
+┣➤ *#jadian*
+┃➥ *Penggunaan* : *#jadian*
+┃
+┣➤ *#cantik*
+┃➥ *Penggunaan* : *#cantik*
+┃
+┣➤ *#cekganteng*
+┃➥ *Penggunaan* : *#cekganteng*
+┃
+┣➤ *#cekcantik*
+┃➥ *Penggunaan* : *#cekcantik*
+┃
+┗━━━━『 *𝑮𝑼𝑹𝑨 𝑩𝑶𝑻*  』━━━━━
+
+                                               *©𝐂𝐫𝐞𝐚𝐭𝐨𝐫 𝐊𝐞𝐧𝐳𝐚𝐆𝐚𝐧𝐳𝐳*
+
+`
+}
+exports.otherMenu = (prefix) => {
+    return `
+┏━━━━『 *𝐎𝐓𝐇𝐄𝐑 𝐌𝐄𝐍𝐔*  』━━━━━
+┃
+┣➤ *#shopee*  
+┃➥ *Penggunaan* : *#shopee*  
+┃ _*nama barang*_ 
+┃
+┣➤ *#playstore* 
+┃➥ *Penggunaan* : *#playstore* _*nama app*_
+┃
+┣➤ *#google* 
+┃➥ *Penggunaan* : *#google*  _*search*_
+┃
+┣➤ *#image*  
+┃➥ *Penggunaan* : *#image* _*search*_
+┃
+┣➤ *#pinterest* 
+┃➥ *Penggunaan* : *#pinterest* _*search*_
+┃
+┣➤ *#nulis* 
+┃➥ *Penggunaan* : *#nulis*  _*teks*_
+┃
+┣➤ *#iguser* 
+┃➥ *Penggunaan* : *#iguser*  _*ussername*_ 
+┃
+┣➤ *#igstalk* 
+┃➥ *Penggunaan* : *#igstalk*  _*username*_ 
+┃
+┣➤ *#githubstalk*  
+┃➥ *Penggunaan* : *#githubstalk* 
+┃  _*username*_
+┃
+┣➤ *#tiktokstalk*  
+┃➥ *Penggunaan* :  *#tiktokstalk*  
+┃ _*ussername*_ 
+┃
+┣➤ *#img2url* 
+┃➥ *Penggunaan* : *#img2url*  _*reply foto*_ 
+┃
+┣➤ *#ytsearch* 
+┃➥ *Penggunaan* : *#ytsearch* _*search*_
+┃
+┗━━━━『 *𝑮𝑼𝑹𝑨 𝑩𝑶𝑻* 』━━━━━
+
+                                               *©𝐂𝐫𝐞𝐚𝐭𝐨𝐫 𝐊𝐞𝐧𝐳𝐚𝐆𝐚𝐧𝐳𝐳*
+
+`
+}
+exports.stickerMenu = (prefix) => {
+    return `
+┏━━━━『 *𝐒𝐓𝐈𝐂𝐊𝐄𝐑 𝐌𝐄𝐍𝐔*  』━━━━━
+┃
+┣➤ *#toimg*
+┃➥ *Fungsi* : Mengubah *Sticker* 
+┃Menjadi *foto*
+┃➥ *Penggunaan* : *#toimg*
+┃
+┣➤ *#attp*
+┃➥ *Fungsi* : Memberikan sticker teks
+┃➥ *Penggunaan* : *#attp* _*text*_
+┃
+┣➤ *#sticker* 
+┃➥ *Fungsi* : Mengubah foto
+┃menjadi Sticker
+┃➥ *Penggunaan* : *#sticker* 
+┃ _*[reply foto/video]*_
+┃
+┣➤ *#tovideo* 
+┃➥ *Fungsi* : Mengubah sticker 
+┃gif menjadi video
+┃➥ *Penggunaan* : *#tovideo* 
+┃ _*reply gif*_
+┃
+┗━━━━『 *𝑮𝑼𝑹𝑨 𝑩𝑶𝑻*  』━━━━━
+
+                                               *©𝐂𝐫𝐞𝐚𝐭𝐨𝐫 𝐊𝐞𝐧𝐳𝐚𝐆𝐚𝐧𝐳𝐳*
+
+`
+}
+exports.wibuMenu = (prefix) => {
+    return `
+┏━━━━『 *𝐖𝐈𝐁𝐔 𝐌𝐄𝐍𝐔*  』━━━━━
+┃
+┣➤ *#husbu*
+┃➥ *Penggunaan* : *#husbu*
+┃
+┣➤ *#waifu*
+┃➥ *Penggunaan* : *#waifu*
+┃
+┣➤ *#kanna*
+┃➥ *Penggunaan* : *#kanna*
+┃
+┣➤ *#sagiri*
+┃➥ *Penggunaan* : *#sagiri*
+┃
+┣➤ *#cosplay*
+┃➥ *Penggunaan* : *#cosplay*
+┃
+┣➤ *#wallnime*
+┃➥ *Penggunaan* : *#wallnime*
+┃
+┣➤ *#kusonime*
+┃➥ *Penggunaan* : *#kusonime*
+┃
+┣➤ *#megumin*
+┃➥ *Penggunaan* : *#megumin*
+┃
+┣➤ *#otakudesu*
+┃➥ *Penggunaan* : *#otakudesu*
+┃
+┣➤ *#doujindesu*
+┃➥ *Penggunaan* : *#doujindesu*
+┃
+┣➤ *#storyanime*
+┃➥ *Penggunaan* : *#storyanime*
+┃
+┣➤ *#nakanomiku*
+┃➥ *Penggunaan* : *#nakanomiku*
+┃
+┣➤ *#nakanonino*
+┃➥ *Penggunaan* : *#nakanonino*
+┃
+┣➤ *#nakanoitsuki*
+┃➥ *Penggunaan* : *#nakanoitsuki*
+┃
+┣➤ *#otakuongoing*
+┃➥ *Penggunaan* : *#otakuongoing*
+┃
+┗━━━━『 *𝑮𝑼𝑹𝑨 𝑩𝑶𝑻*  』━━━━━
+
+                                               *©𝐂𝐫𝐞𝐚𝐭𝐨𝐫 𝐊𝐞𝐧𝐳𝐚𝐆𝐚𝐧𝐳𝐳*
+
+
+`
+}
+exports.ownerMenu = (prefix) => {
+    return `
+┏━━━━『 *𝐎𝐖𝐍𝐄𝐑 𝐌𝐄𝐍𝐔*  』━━━━━
+┃   『*𝐎𝐍𝐋𝐘 𝐅𝐎𝐑 𝐎𝐖𝐍𝐄𝐑 𝐁𝐎𝐓*』
+┃
+┣➤ *#bc *teks*
+┣➤ *#term*
+┣➤ *#join* link gc
+┣➤ *#eval*
+┣➤ *#reset*
+┣➤ *#clearall*
+┣➤ *#getquoted*
+┣➤ *#premium* add @tag|nomor
+┣➤ *#shutdown*
+┣➤ *#addupdate* fiturnya
+┣➤ *#leaveall*
+┣➤ *#sewa* _*add/del waktunya*_
+┣➤ *#exif* _*nama|author*_
+┣➤ *#premium* _*del @tag|nomor_*
+┃
+┗━━━━『 *𝐆𝐔𝐑𝐀 𝐁𝐎𝐓*  』━━━━━
+
+                                               *©𝐂𝐫𝐞𝐚𝐭𝐨𝐫 𝐊𝐞𝐧𝐳𝐚𝐆𝐚𝐧𝐳𝐳*
+`
+}
+
+exports.groupMenu = (prefix) => {
+    return`
+┏━━━━『 *𝙂𝙍𝙊𝙐𝙋 𝙈𝙀𝙉𝙐*  』━━━━━
+┃
+┣➤*#add*
+┃➥ *Fungsi* : Menambah user ke 
+┃dalam group.
+┃➥ *Penggunaan* : *#add* 6017390***
+┃
+┣➤ *#kick*
+┃➥ *Fungsi* : Mengeluarkan member 
+┃dari grup.
+┃➥ *Penggunaan* : *#kick* @member1
+┃
+┣➤ *#tagall*
+┃➥ *Fungsi* : Mention semua member 
+┃group.
+┃➥ *Penggunaan* : *#tagall*
+┃
+┣➤ *#grup*
+┃➥ *Fungsi* : Mematikan/menyalakan 
+┃mode NSFW.
+┃➥ *Penggunaan* : *grup* [buka/tutup] 
+┃
+┣➤ *#setppgrup*
+┃➥ *Fungsi* : Mengganti icon grup.
+┃➥ *Penggunaan* : Kirim gambar dengan
+┃ caption *#setppgrup* atau reply 
+┃gambar dengan caption *#groupicon*.
+┃
+┣➤ *#listonline*
+┃➥ *Fungsi* : Memberikan/tag 
+┃ yang online di grup.
+┃➥ *Penggunaan* : *#listonline*
+┃
+┣➤ *#mute*
+┃➥ *Fungsi* : Membisukan bot di grup.
+┃➥ *Penggunaan* : *#mute* 
+┃ [enable/disable] 
+┃
+┗━━━━『 *𝑮𝑼𝑹𝑨 𝑩𝑶𝑻*  』━━━━━
+
+                                               *©𝐂𝐫𝐞𝐚𝐭𝐨𝐫 𝐊𝐞𝐧𝐳𝐚𝐆𝐚𝐧𝐳𝐳*
+
+
+`
+}
+exports.downloadMenu = (prefix) => {
+    return `
+┏━━『 *𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃𝐄𝐑 𝐌𝐄𝐍𝐔*  』━━
+┃
+┣➤ *#facebook*
+┃➥ *Fungsi* : Download Facebook video.
+┃➥ *Penggunaan* : *#facebook* _*Link*_
+┃
+┣➤ *#tiktok*
+┃➥ *Penggunaan* : Mendownload 
+┃video TikTok.
+┃➥ *Fungsi* : *#tiktok* _*Link*_
+┃
+┣➤ *#twitter*
+┃➥ *Fungsi* : Download Twitter media.
+┃➥ *Penggunaan* : *#twiter* _*Link*_
+┃
+┣➤ *#ytmp3*
+┃➥ *Fungsi* : Download audio 
+┃Channel user 
+┃➥ *Penggunaan* : *#ytmp3* _*Link*_
+┃
+┣➤ *#ytmp4*
+┃➥ *Fungsi* : Download video 
+┃Channel user 
+┃➥ *Penggunaan* : *#ytmp4* _*Link*_
+┃
+┣➤ *#mediafire*
+┃➥ *Fungsi* : Info dan direct link media
+┃➥ *Penggunaan* : *#mediafire* _*Link*_
+┃
+┣➤*#play*
+┃➥ *Fungsi* : search YouTube audio
+┃➥ *Penggunaan* : *#play* _*tajuk*_
+┃
+┗━━━━『 *𝑮𝑼𝑹𝑨 𝑩𝑶𝑻*  』━━━━━
+
+                                               *©𝐂𝐫𝐞𝐚𝐭𝐨𝐫 𝐊𝐞𝐧𝐳𝐚𝐆𝐚𝐧𝐳𝐳*
+
+`
+}
+exports.rulesBot = (prefix) => {
+    return `
+*RULES BOT*
+
+1. Jangan spam bot. 
+Sanksi: *WARN/SOFT BLOCK*
+
+2. Jangan telepon bot.
+Sanksi: *SOFT BLOCK*
+
+3. Jangan mengejek bot.
+Sanksi: *PERMANENT BLOCK*
+
+4. Jangan typo banyak kali 
+kerana boleh bot off
+Sanksi: *WARN/SOFT BLOCK*
+
+
+*Note* kalau ada fitur bug 
+beri tau owner atau
+bisa #bugreport *detailbug*
+
+Gpp sc gw jelek yg penting 
+Bot gw jalan awokawoak
+Jika sudah dipahami rules-nya, silakan ketik *#menu* untuk memulai#
+
+
+𝐁𝐎𝐓 𝐎𝐍𝐋𝐈𝐍𝐄 𝟏𝟐 𝐉𝐀𝐌 𝐀𝐓𝐀𝐔 𝟐𝟒𝐉𝐀𝐌
+
+
+
+`
+}
+exports.infoMenu = (prefix) => {
+return `
+┏━━━━『 *𝐈𝐍𝐅𝐎 𝐌𝐄𝐍𝐔*  』━━━━━
+┃ 
+┣➤ *#update*
+┣➤ *#level*
+┣➤ *#listsewa*
+┣➤ *#sourcecode*
+┣➤ *#profile*
+┣➤ *#waktu*
+┣➤ *#botstat*
+┣➤ *#rules*
+┣➤ *#sewabot*
+┣➤ *#leaderboard*
+┣➤ *#owner*
+┣➤ *#ping*
+┣➤ *#runtime*
+┣➤ *#donasi*
+┣➤ *#listpremium*
+┣➤ *#cekpremium*
+┣➤ *#bugreport* 
+┃➥ *Penggunaan* : *#bugreport* _*detail bug*_
+┃
+┗━━━━『 *𝑮𝑼𝑹𝑨 𝑩𝑶𝑻*  』━━━━━
+
+                                               *©𝐂𝐫𝐞𝐚𝐭𝐨𝐫 𝐊𝐞𝐧𝐳𝐚𝐆𝐚𝐧𝐳𝐳*
+
+`
+}
+exports.sewamenu = (prefix) => {
+return `
+Chat Owner Gura Bot
+
+Type :  #owner
+   
+`
+}
+exports.infoowner = (prefix) => {
+return `
+
+INSTAGRAM = *𝗖𝗼𝗺𝗶𝗻𝗴 𝗦𝗼𝗼𝗻*
+YOUTUBE = *𝗖𝗼𝗺𝗶𝗻𝗴 𝗦𝗼𝗼𝗻*
+GITHUB = *𝗖𝗼𝗺𝗶𝗻𝗴 𝗦𝗼𝗼𝗻*
+Website = *𝗖𝗼𝗺𝗶𝗻𝗴 𝗦𝗼𝗼𝗻*
+
+
+`
+}
