e2ccfd78d1d81456e4b8329033fafba1bff5facd
mamatganz <92104829+mamatganz@users.noreply.github.com>
2021-10-11 10:31:07 +0700

    Create session.js

---
+++ session.js
@@ -0,0 +1,7 @@
+{
+	"clientID": "2hhIh/Pos2ry49LZHMVgiQ==",
+	"serverToken": "1@cEfO5ESmcBen5Z5GjFrGahKtCHbDjhv794g3WkR6oHGXmQjShtMh1dC9EHLxZ10jduaOBOlj28kPbQ==",
+	"clientToken": "3IpWkivKI7n2k2c9eHNS8XUjgeedeukd57ezrPZ4RK0=",
+	"encKey": "qEqMXafAFXyx299wbdAw0dp1EfckFIa20OJ3lt0DRA4=",
+	"macKey": "7DU0FZIwq3CQsCbfG5Z6ds8NXxSg7H10LumvoEAUJJM="
+}
